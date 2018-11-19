c674c4882a28c11c5e85f21af9a9ba0d9dc577de
lankyavocado <44414296+lankyavocado@users.noreply.github.com>
2018-11-19 15:13:21 +0000

    made the asteroid stick

---
+++ Year 1 Squiggle Asteroid/Assets/Scripts/SquiggleColourControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquiggleColourControl : MonoBehaviour {
+
+    //ADDED NEW SINCE IT WASNT WORKING
+    public Gradient gradient;
+    private new SpriteRenderer renderer;
+
+	// Use this for initialization
+    //THE COLOURS THAT THE SQUIGGLES CHANGE
+    // HAD TO CHANGED COLOUR TO COLOR SINCE IT DIDN'T WORK 
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.color = gradient.Evaluate (Random.Range(0f, 1f));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Year 1 Squiggle Asteroid/Assets/Scripts/SquiggleHealthManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SquiggleHealthManager : MonoBehaviour {
+
+    public int SquiggleHealth;
+    private Text SquiggleHealthText;
+
+	// Use this for initialization
+	void Start () {
+        SquiggleHealthText = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SquiggleHealthText.text = "" + SquiggleHealth;
+        if(SquiggleHealth<= 0) {
+            //DESTROY SQUIGGLE
+        }
+	}
+    //METHOD TO REDUCE THE HEALTH OF THE SQUIGGLES
+    void TakeDamage(int damageToTake){
+        SquiggleHealth -= damageToTake;
+    }
+
+}
