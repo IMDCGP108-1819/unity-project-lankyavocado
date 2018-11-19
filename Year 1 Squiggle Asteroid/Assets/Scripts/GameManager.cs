811bc61f6b0c3ad5682911ede8d14ed4b07ba02e
lankyavocado <44414296+lankyavocado@users.noreply.github.com>
2018-11-19 16:04:14 +0000

    made squiggles generate randomly

--- Year 1 Squiggle Asteroid/Assets/Scripts/GameManager.cs
+++ Year 1 Squiggle Asteroid/Assets/Scripts/GameManager.cs
@@ -6,14 +6,26 @@ public class GameManager : MonoBehaviour {
 
     public Transform[] spawnPoints;
     public GameObject Squiggle;
+    public int numberOfSquigglesToStart;
 
 	// Use this for initialization
 	void Start () {
+        int numberOfSquigglesCreate = 0;
+
 		//CREATES SQUIGGLES ON THE SPAWN POINTS
-        for(int i = 0; i < spawnPoints.Length; i++) {
-            Instantiate(Squiggle, spawnPoints[i].position, Quaternion.identity);
+        if(numberOfSquigglesCreate < numberOfSquigglesToStart) {
+            for (int i = 0; i < spawnPoints.Length; i++) {
+                int SquiggleToCreate = Random.Range(0, 2);
+                if (SquiggleToCreate == 0) {
+                    Instantiate(Squiggle, spawnPoints[i].position, Quaternion.identity);
+                    //CHANGES THE NUMBER OF SQUIGGLES THAT SPAWN EACH TIME
+                    
+                    numberOfSquigglesCreate++;
+                }
+            }
         }
 	}
+    //PART 9 
 	
 	// Update is called once per frame
 	void Update () {
